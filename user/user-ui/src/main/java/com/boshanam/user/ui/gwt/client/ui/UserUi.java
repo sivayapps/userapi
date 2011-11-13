98b70de81a90470692860e3fc121ad42745c2dd7
sivayapps@gmail.com <sivayapps@gmail.com@a591595a-121e-4dd0-f102-539eddd48f45>
2011-11-13 10:19:12 +0000

    git-svn-id: https://boshanam.googlecode.com/svn/trunk@5 a591595a-121e-4dd0-f102-539eddd48f45

--- user/user-ui/src/main/java/com/boshanam/user/ui/gwt/client/ui/UserUi.java
+++ user/user-ui/src/main/java/com/boshanam/user/ui/gwt/client/ui/UserUi.java
@@ -2,12 +2,16 @@ package com.boshanam.user.ui.gwt.client.ui;
 
 import com.boshanam.user.ui.core.gwt.client.ui.UserApplicationView;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class UserUi implements EntryPoint {
 
 	public void onModuleLoad() {
 		UserApplicationView view = new UserApplicationView();
 
+		// hide the animated 'loading.gif'
+		RootPanel.get("loading").setVisible(false);
+		
 		view.show();
 
 	}
