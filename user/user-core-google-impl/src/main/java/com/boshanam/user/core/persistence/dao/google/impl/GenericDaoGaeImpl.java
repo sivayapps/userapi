bf35ef792a680aa58f781a1b0a998521e77ae01e
sivayapps <sivayapps@gmail.com>
2012-01-24 23:59:41 +0530

    1 - Enh - Extract Generic DAO implementation that can be resued by specific daos

---
+++ user/user-core-google-impl/src/main/java/com/boshanam/user/core/persistence/dao/google/impl/GenericDaoGaeImpl.java
@@ -0,0 +1,252 @@
+/**
+ * 
+ */
+package com.boshanam.user.core.persistence.dao.google.impl;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.orm.jpa.JpaCallback;
+import org.springframework.orm.jpa.support.JpaDaoSupport;
+
+import com.boshanam.user.core.model.IDomainObject;
+import com.boshanam.user.core.persistence.dao.IGenericDao;
+
+/**
+ * @author Siva
+ * @Email: shiva.forums@gmail.com
+ * @Date Jan 24, 2012 7:50:42 PM
+ * 
+ */
+public class GenericDaoGaeImpl<T extends IDomainObject, ID extends Serializable> extends JpaDaoSupport implements IGenericDao<T, ID> {
+
+	private static Logger sLogger = LoggerFactory.getLogger(GenericDaoGaeImpl.class);
+
+	protected Class<T> persistentClass;
+
+	/**
+	 * This is default constructor used when this generic dao is extended by
+	 * specific dao's.
+	 */
+	@SuppressWarnings("unchecked")
+	public GenericDaoGaeImpl() {
+		this.persistentClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		sLogger = LoggerFactory.getLogger(this.persistentClass);
+	}
+
+	/**
+	 * This constructor is expected to be used when instantiating this generic dao
+	 * instead of specific one. This is generally used in spring bean definitions,
+	 * (use as an abstract bean definition and extended in each specific case by
+	 * passing constructor-arg's, this will simplify the code by reusing generic
+	 * dao implementation instead of creating specific dao's, we can use generic
+	 * one in specific cases as well.
+	 * 
+	 * At the same time this will simplify the spring bean definitions by creating
+	 * abstract bean definition with generic dao at once and extending that bean
+	 * definition and creating specific bean definitions by passing
+	 * constructor-arg as specific entity class name).
+	 * 
+	 * @param persistentClass
+	 */
+	public GenericDaoGaeImpl(final Class<T> persistentClass) {
+		super();
+		this.persistentClass = persistentClass;
+		sLogger = LoggerFactory.getLogger(this.persistentClass);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#exists(java.io.Serializable
+	 * )
+	 */
+	@Override
+	public boolean exists(ID id) {
+		return (id != null) ? (this.getJpaTemplate().find(this.persistentClass, id) != null) : false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.boshanam.user.core.persistence.dao.IGenericDao#findById(java.io.
+	 * Serializable)
+	 */
+	@Override
+	public T findById(ID id) {
+		return this.getJpaTemplate().find(this.persistentClass, id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.boshanam.user.core.persistence.dao.IGenericDao#findById(java.io.
+	 * Serializable, java.lang.Integer)
+	 */
+	@Override
+	public T findById(ID id, Integer joinSizeFetchMode) {
+		// TODO Auto-generated method stub
+		sLogger.warn("findById(ID id, Integer joinSizeFetchMode), This operation is Not yet implemented");
+		throw new UnsupportedOperationException("Not yet implemented");
+		// return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.boshanam.user.core.persistence.dao.IGenericDao#findAll()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findAll() {
+		return this.getJpaTemplate().executeFind(new JpaCallback<List<T>>() {
+			public List<T> doInJpa(EntityManager em) throws PersistenceException {
+				Query q = em.createQuery("SELECT p FROM " + persistentClass.getName() + " p");
+				List<T> result = (List<T>) q.getResultList();
+				if (result != null && result.size() > 0) {
+					return result;
+				} else {
+					return Collections.emptyList();
+				}
+			}
+
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#findAll(java.lang.Integer
+	 * )
+	 */
+	@Override
+	public List<T> findAll(Integer joinSizeFetchMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#persist(java.lang.Object
+	 * )
+	 */
+	@Override
+	public void persist(T entity) {
+		this.getJpaTemplate().persist(entity);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#persistEntityId(java
+	 * .lang.Object)
+	 */
+	@Override
+	public ID persistEntityId(T entity) {
+		this.getJpaTemplate().persist(entity);
+		this.getJpaTemplate().flush();
+		return (ID) entity.getId();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#create(java.lang.Object)
+	 */
+	@Override
+	public T create(T entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#update(java.lang.Object)
+	 */
+	@Override
+	public T update(T entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#merge(java.lang.Object)
+	 */
+	@Override
+	public T merge(T entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#mergeAndPersist(java
+	 * .lang.Object)
+	 */
+	@Override
+	public void mergeAndPersist(T entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.boshanam.user.core.persistence.dao.IGenericDao#remove(java.lang.Object)
+	 */
+	@Override
+	public void remove(T entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.boshanam.user.core.persistence.dao.IGenericDao#removeId(java.io.
+	 * Serializable)
+	 */
+	@Override
+	public ID removeId(ID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @return the persistentClass
+	 */
+	public Class<T> getPersistentClass() {
+		return persistentClass;
+	}
+
+	/**
+	 * @param persistentClass
+	 *          the persistentClass to set
+	 */
+	public void setPersistentClass(Class<T> persistentClass) {
+		this.persistentClass = persistentClass;
+	}
+
+}
--- user/user-core-google-impl/src/main/java/com/boshanam/user/core/persistence/dao/google/impl/PersonDaoGoogleImpl.java
+++ user/user-core-google-impl/src/main/java/com/boshanam/user/core/persistence/dao/google/impl/PersonDaoGoogleImpl.java
@@ -13,7 +13,6 @@ import javax.persistence.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.orm.jpa.JpaCallback;
-import org.springframework.orm.jpa.support.JpaDaoSupport;
 
 import com.boshanam.user.core.model.entities.Person;
 import com.boshanam.user.core.persistence.dao.IPersonDao;
@@ -24,94 +23,9 @@ import com.boshanam.user.core.persistence.dao.IPersonDao;
  * @Date Jan 8, 2012 2:33:18 AM
  * 
  */
-public class PersonDaoGoogleImpl extends JpaDaoSupport implements IPersonDao<Person> {
+public class PersonDaoGoogleImpl extends GenericDaoGaeImpl<Person, Long> implements IPersonDao<Person> {
 
 	private static Logger sLogger = LoggerFactory.getLogger(PersonDaoGoogleImpl.class);
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.boshanam.user.core.persistence.dao.GenericDao#exists(java.io.Serializable
-	 * )
-	 */
-	public boolean exists(Long id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.boshanam.user.core.persistence.dao.GenericDao#findById(java.io.
-	 * Serializable)
-	 */
-	public Person findById(Long id) {
-
-		Person p = this.getJpaTemplate().find(Person.class, id);
-		if (p != null) {
-			return p;
-		}
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.boshanam.user.core.persistence.dao.GenericDao#findById(java.io.
-	 * Serializable, java.lang.Integer)
-	 */
-	public Person findById(Long id, Integer joinSizeFetchMode) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.boshanam.user.core.persistence.dao.GenericDao#findAll()
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Person> findAll() {
-		return this.getJpaTemplate().executeFind(new JpaCallback<List<Person>>() {
-			public List<Person> doInJpa(EntityManager em) throws PersistenceException {
-				Query q = em.createQuery("SELECT p FROM com.boshanam.user.core.model.entities.Person p");
-				List<Person> result = (List<Person>) q.getResultList();
-
-				if (result != null && result.size() > 0) {
-
-					return result;
-				} else {
-					return Collections.emptyList();
-				}
-			}
-
-		});
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.boshanam.user.core.persistence.dao.GenericDao#findAll(java.lang.Integer
-	 * )
-	 */
-	public List<Person> findAll(Integer joinSizeFetchMode) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.boshanam.user.core.persistence.dao.GenericDao#persist(java.lang.Object
-	 * )
-	 */
-	public void persist(Person entity) {
-		throw new UnsupportedOperationException("Not yet implemented siva TODO");
-		// this.getJpaTemplate().persist(entity);
-	}
 
 	/*
 	 * (non-Javadoc)
