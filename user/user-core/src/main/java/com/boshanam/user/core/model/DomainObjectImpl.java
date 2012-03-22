c5856ba3ae6569630cee0a6f098769ea6dea71e7
sivayapps <sivayapps@gmail.com>
2012-03-23 00:55:10 +0530

    15 - Task - Implement hashCode and equals for all domain and dto classes based on ID

--- user/user-core/src/main/java/com/boshanam/user/core/model/DomainObjectImpl.java
+++ user/user-core/src/main/java/com/boshanam/user/core/model/DomainObjectImpl.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
 
+import com.boshanam.user.core.dto.ObjectDto;
+
 public abstract class DomainObjectImpl<ID extends Serializable> implements IDomainObject<ID> {
 
 	public abstract ID getId();
@@ -66,4 +68,51 @@ public abstract class DomainObjectImpl<ID extends Serializable> implements IDoma
 		}
 		return false;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		// here if id is null, then we are falling back to native hash code of
+		// Object as we don't want two DTO/Entities without id assigned to be
+		// treated as
+		// equal. If we return different hash code for different entities
+		result = prime * result + ((getId() == null) ? super.hashCode() : getId().hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ObjectDto other = (ObjectDto) obj;
+		if (getId() == null) {
+			// here we have to treat objects as unequal if any of the ID is NULL or
+			// even both Entitie's ID's are NULL, this allows us to store more than
+			// one Transient (Entity objects not yet persisted) into Set or Hash based
+			// collections.
+			return false;
+		} else if (!getId().equals(other.getId())) {
+			return false;
+		}
+		return true;
+	}
+
 }
